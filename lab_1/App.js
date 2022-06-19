6c0397be196a54fa9922b52d4fcb4091eb0ad6ca
Nguyen Dang Minh <83014500+dangminh71220002@users.noreply.github.com>
2022-06-20 00:30:23 +0700

    Flex Screen
    divide Screen

--- lab_1/App.js
+++ lab_1/App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.midBody}>
+        <Text>
+          MidBody
+        </Text>
+      </View>
     </View>
   );
 }
@@ -16,5 +24,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header:{
+    flex: 10,
+    backgroundColor:'Blue',
+  },
+  midBody:{
+    flex:90,
   },
 });
