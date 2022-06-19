86b2e88f3c03e83a15893e7a997994ccb0ee121b
Nguyen Dang Minh <83014500+dangminh71220002@users.noreply.github.com>
2022-06-20 00:47:07 +0700

    Button
    Add Button

--- lab_1/App.js
+++ lab_1/App.js
@@ -1,18 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+import { Button } from 'react-native-web';
+import React, { useState } from "react";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
+        <TouchableOpacity style={styles.buttonHeader}>
+          <Text  style={{backgroundColor:'yellow'}}> Back </Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.midBody}>
-        <Text>
-          MidBody
-        </Text>
+        <TouchableOpacity style={styles.distanceButton}> 
+          <Text style={{fontSize:30}}>1h</Text>  
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.distanceButton}> 
+          <Text style={{fontSize:30}}>2h</Text>  
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.distanceButton}> 
+          <Text style={{fontSize:30}}>3h</Text>  
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.distanceButton}> 
+          <Text style={{fontSize:30}}>4h</Text>  
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -21,15 +32,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   header:{
     flex: 10,
-    backgroundColor:'Blue',
+    backgroundColor:'blue',
+    color:"blue",
+    // alignItems: 'center',
+     justifyContent: 'center',
   },
   midBody:{
     flex:90,
+    backgroundColor:'red',
+  },
+  distanceButton:{
+    marginTop:'5%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonHeader: {
+    width:'20%',
+    height:'60%',
+    fontSize:30,
+    marginLeft:'10%'
   },
 });
